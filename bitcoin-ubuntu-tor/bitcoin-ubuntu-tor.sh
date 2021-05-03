d6e781ffc9afb2c4dde47e114ffa7ee7cd78c868
bavarianledger <48831982+bavarianledger@users.noreply.github.com>
2021-05-03 10:29:41 +0200

    Updated to 0.21.1
    updated to V3 onion nodes
    torified downloads

--- bitcoin-ubuntu-tor/bitcoin-ubuntu-tor.sh
+++ bitcoin-ubuntu-tor/bitcoin-ubuntu-tor.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-bitcoinVersion="0.21.0"
+bitcoinVersion="0.21.1"
 
 cd ~
 sudo apt update
-sudo apt install tor
-sudo unattended-upgrade
+sudo apt install tor wget -y
+sudo dist-upgrade -y
 sudo rm -Rf bitcoincoredownload
 mkdir bitcoincoredownload
 cd bitcoincoredownload
 
-wget https://bitcoincore.org/bin/bitcoin-core-${bitcoinVersion}/bitcoin-${bitcoinVersion}-x86_64-linux-gnu.tar.gz
-wget https://bitcoin.org/bin/bitcoin-core-${bitcoinVersion}/SHA256SUMS.asc
+torify wget https://bitcoincore.org/bin/bitcoin-core-${bitcoinVersion}/bitcoin-${bitcoinVersion}-x86_64-linux-gnu.tar.gz
+torify wget https://bitcoin.org/bin/bitcoin-core-${bitcoinVersion}/SHA256SUMS.asc
 tar xvzf bitcoin-${bitcoinVersion}-x86_64-linux-gnu.tar.gz
 
 sha256sum --ignore-missing --check SHA256SUMS.asc
@@ -22,4 +22,11 @@ sudo install -m 0755 -o root -g root -t /usr/local/bin bitcoin-${bitcoinVersion}
 
 mkdir ~/.bitcoin
 # onion nodes copied from https://github.com/rootzoll/raspiblitz/
-echo -e "proxy=127.0.0.1:9050\nserver=1\nlisten=1\nbind=127.0.0.1\nonlynet=onion\ndnsseed=0\ndns=0\nblockfilterindex=1\naddnode=fno4aakpl6sg6y47.onion\naddnode=toguvy5upyuctudx.onion\naddnode=ndndword5lpb7eex.onion\naddnode=6m2iqgnqjxh7ulyk.onion\naddnode=5tuxetn7tar3q5kp.onion\naddnode=juo4oneckybinerq.onion" > ~/.bitcoin/bitcoin.conf
+echo -e "proxy=127.0.0.1:9050\nserver=1\nlisten=1\nbind=127.0.0.1\nonlynet=onion\ndnsseed=0\ndns=0\nblockfilterindex=1\n" > ~/.bitcoin/bitcoin.conf
+echo "main.bind=127.0.0.1" >> ~/.bitcoin/bitcoin.conf
+echo "main.addnode=ira7kqcbff52wofoong2dieh2xlvmw4e7ya3znsqn7wivn6armetvrqd.onion" >> ~/.bitcoin/bitcoin.conf
+echo "main.addnode=xlpi353v7ia5b73msynr7tmddgxoco7n2r2bljt5txpv6bpzzphkreyd.onion" >> ~/.bitcoin/bitcoin.conf
+echo "main.addnode=ccjrb6va3j6re4lg2lerlt6wyvlb4tod7qbe7rwiouuapb7etvterxyd.onion" >> ~/.bitcoin/bitcoin.conf
+echo "main.addnode=s7m4mnd6bokujhywsocxibispktruormushdroeaeqeb3imvztfs3vid.onion" >> ~/.bitcoin/bitcoin.conf
+echo "main.addnode=ldvhlpsrvspquqnl3gutz7grfu5lb3m2dgnezpl3tlkxgpoiw2g5mzid.onion" >> ~/.bitcoin/bitcoin.conf
+echo "main.addnode=gliovxxzyy2rkwaoz25khf6oa64c3csqzjn3t6dodsjuf34w6a6ktsyd.onion" >> ~/.bitcoin/bitcoin.conf
